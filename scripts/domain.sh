6f58ecf26b1bf0121dd71666220adb90584efef8
UserBulba <foxxxiu@gmail.com>
2024-07-06 20:08:11 +0200

    update: script rename, script path changed

--- scripts/domain.sh
+++ scripts/domain.sh
@@ -3,7 +3,7 @@ set -e  # Exit on error
 
 # Default values for the Docker container name and the path to the Python script
 CONTAINER_NAME="${1:-tor}"
-SCRIPT_PATH="${2:-/etc/tor/scripts/get_hidden_domain.py}"
+SCRIPT_PATH="${2:-/etc/tor/scripts/get_tor_hidden_domain.py}"
 
 # Executing the Python script inside the specified Docker container to get the .onion domain.
 domain=$(docker exec "$CONTAINER_NAME" python "$SCRIPT_PATH")
