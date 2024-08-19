4b6fb96f501365a5ee960ae799b0d12ec9f60fea
UserBulba <foxxxiu@gmail.com>
2024-08-19 22:49:40 +0200

    fix: Nyx default container, add param to specify others.

--- scripts/nyx.sh
+++ scripts/nyx.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e  # Exit on error
 
-# Find the container ID of the running tor:octo container
-TOR_CONTAINER_ID=$(docker ps --filter "ancestor=tor:octo" --format "{{.ID}}")
+# Default value for the Docker container name
+CONTAINER_NAME="${1:-tor-frontend}"
 
 # Check if the container is found and running
-if [ -z "$TOR_CONTAINER_ID" ]; then
-    echo "Error: No running container found for image tor:octo"
+if [ -z "$CONTAINER_NAME" ]; then
+    echo "Error: No running container found for image '$CONTAINER_NAME'."
     exit 1
 fi
 
@@ -15,7 +15,7 @@ echo "=============================="
 echo "In the darkness of the Underworld, Nyx, the Night Incarnate, watches over..."
 echo "=============================="
 
-docker exec -it "$TOR_CONTAINER_ID" sh -c 'PYTHONWARNINGS="ignore:invalid escape sequence" nyx'
+docker exec -it "$CONTAINER_NAME" sh -c 'PYTHONWARNINGS="ignore:invalid escape sequence" nyx'
 
 echo "======================================"
 echo "The night recedes. Until next time, traveler."
