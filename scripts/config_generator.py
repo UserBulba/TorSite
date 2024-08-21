4114162a287ea634b8b7914b9a2d422fdc56cd72
UserBulba <foxxxiu@gmail.com>
2024-08-21 23:13:56 +0200

    Kuma: add config generator

--- scripts/config_generator.py
+++ scripts/config_generator.py
@@ -5,7 +5,17 @@ from jinja2 import Environment
 from jinja2 import FileSystemLoader
 
 
-def generate_ob_config(env: Environment, master_onion_address: str):
+def generate_ob_config(env: Environment, master_onion_address: str) -> None:
+    """
+    Generate the ob_config file for Tor services.
+
+    Args:
+        env (Environment): Jinja2 Environment object.
+        master_onion_address (str): The master onion address to be used in the config file.
+
+    Returns: None
+    """
+
     try:
         template = env.get_template("ob_config.template")
         output = template.render(master_onion_address=master_onion_address)
@@ -22,9 +32,21 @@ def generate_config(
     log_location: str,
     domains: List[str],
     key_path: str,
-):
+) -> None:
+    """
+    Generate the config.yaml file for OnionBalance.
 
-    # Displaying the domains for verification before processing
+    Args:
+        env (Environment): Jinja2 Environment object.
+        log_level (str): Log level for the config.
+        log_location (str): Log file location for the config.
+        domains (List[str]): List of backend domains for the config.
+        key_path (str): Path to the secret key file for the config.
+
+    Returns: None
+    """
+
+    # Displaying the domains before processing.
     print("Processing the following domains:")
     for domain in domains:
         print(domain)
@@ -44,6 +66,26 @@ def generate_config(
         print(f"An error occurred while generating config.yaml: {e}")
 
 
+def generate_monitor_config(env: Environment, master_onion_address: str) -> None:
+    """
+    Generate the monitor_config.sql file for Kuma.
+
+    Args:
+        env (Environment): Jinja2 Environment object.
+        master_onion_address (str): The master onion address to be used in the config file.
+
+    Returns: None
+    """
+    try:
+        template = env.get_template("monitor_config.template")
+        output = template.render(master_onion_address=master_onion_address)
+        with open("bin/monitor/scripts/monitor_config.sql", "w") as conf:
+            conf.write(output)
+        print("monitor_config generated successfully.")
+    except Exception as e:
+        print(f"An error occurred while generating monitor_config: {e}")
+
+
 def main(args: argparse.Namespace):
     env = Environment(loader=FileSystemLoader("scripts/templates"))
 
@@ -55,6 +97,9 @@ def main(args: argparse.Namespace):
             env, args.log_level, args.log_location, args.domains, args.key_path
         )
 
+    if args.config_type == "monitor_config" or args.config_type == "all":
+        generate_monitor_config(args.master_onion_address)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -62,7 +107,7 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "config_type",
-        choices=["ob_config", "config", "all"],
+        choices=["ob_config", "config", "monitor_config", "all"],
         help="Type of configuration to generate.",
     )
     parser.add_argument(
@@ -85,9 +130,11 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    # Ensure that master_onion_address is provided for ob_config
+    # Ensure that master_onion_address is provided.
     if (
-        args.config_type == "ob_config" or args.config_type == "all"
+        args.config_type == "ob_config"
+        or args.config_type == "monitor_config"
+        or args.config_type == "all"
     ) and not args.master_onion_address:
         parser.error(
             "The --master_onion_address argument is required for 'ob_config' or 'all'"
